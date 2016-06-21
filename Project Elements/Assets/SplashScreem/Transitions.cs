9caf51c4d1f6e5abdca705dfabc3986381c5a1dc
Ormanus <osalminenster@gmail.com>
2016-06-21 09:16:07 +0300

    Added splash screen and a horrible logo placeholder

---
+++ Project Elements/Assets/SplashScreem/Transitions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Transitions: MonoBehaviour {
+
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > 2.0f)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+	}
+}
