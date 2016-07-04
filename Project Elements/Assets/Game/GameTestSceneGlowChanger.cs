9a3759fa48c6654c6f83f5f61cfe3c8e03eb6f62
miccoa <miccoa@hotmail.com>
2016-07-04 14:51:41 +0300

    glow cntd.

---
+++ Project Elements/Assets/Game/GameTestSceneGlowChanger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameTestSceneGlowChanger : MonoBehaviour {
+	Color oldcolor;
+
+	// Use this for initialization
+	void Start () {
+		oldcolor = gameObject.GetComponent<SpriteRenderer> ().color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown(KeyCode.M)) {
+			//pausemode = pause ();
+		
+		//arvo = 20;
+			Debug.Log ("no mo");
+			gameObject.GetComponent<SpriteRenderer> ().color = oldcolor;
+		}
+		if (Input.GetKeyDown(KeyCode.K)) {
+			//pausemode = pause ();
+
+			//arvo = 20;
+			Debug.Log ("no mo2");
+			gameObject.GetComponent<SpriteRenderer> ().color = Color.black;
+		}
+
+	}
+
+}
