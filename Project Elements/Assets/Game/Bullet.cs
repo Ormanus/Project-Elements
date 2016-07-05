f76d74c05e41860f782c9c8673c0aa96bc65a984
roopekk <roopekauhanen00@gmail.com>
2016-07-05 14:06:53 +0300

    Shoot Fix

--- Project Elements/Assets/Game/Bullet.cs
+++ Project Elements/Assets/Game/Bullet.cs
@@ -5,25 +5,34 @@ public class Bullet : MonoBehaviour {
 
     Rigidbody2D rb;
     public float amount;
-    public Vector3 sp;
-    public Vector3 dir;
+    //public Vector3 sp;
+    //public Vector3 dir;
+
+    public Transform Bulletspawn;
+    public Vector2 shoot;
     //public GameObject ShootDirection;
     
     // Use this for initialization
     void Start() {
 
         //ShootDirection = GameObject.FindGameObjectWithTag("ShootDirection");
+
+        Bulletspawn = GameObject.Find("BulletSpawn").transform;
+
+        shoot = Bulletspawn.transform.up;
+
         rb = GetComponent<Rigidbody2D>();
 
-         sp = Camera.main.WorldToScreenPoint(transform.position);
-        dir = (Input.mousePosition - sp).normalized;
+
+        // sp = Camera.main.WorldToScreenPoint(transform.position);
+        //dir = (Input.mousePosition - sp).normalized;
 }
 
 	
 	// Update is called once per frame
 	void Update () {
         //rb.velocity = rb.velocity = Vector2.up;      
-        rb.AddForce(dir * amount);
+        rb.AddForce(shoot*amount);
         Destroy(gameObject,5);
     }
 }
