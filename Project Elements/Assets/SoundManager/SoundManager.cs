0b918eb1dd02b7ed3231744fbf1590fdac455aeb
miccoa <miccoa@hotmail.com>
2016-07-15 11:27:31 +0300

    Music volume adjust
    not in the options yet

---
+++ Project Elements/Assets/SoundManager/SoundManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+	public AudioSource music;
+
+	// Use this for initialization
+	void Start () {
+		
+		//music.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
