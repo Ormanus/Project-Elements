6c7e54b20a3853addcd56122949608233a0cfdb1
roopekk <roopekauhanen00@gmail.com>
2016-07-20 12:20:08 +0300

    Update

---
+++ Project Elements/Assets/Game/Rotation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 relativePos = (target.position + new Vector3(0, 0.5f, 0)) - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+        Quaternion current = transform.localRotation;
+
+        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime * 4);
+        transform.Translate(0, 0, 10 * Time.deltaTime);
+        //transform.RotateAround(target.position,Vector3.up,180*Time.deltaTime);
+    }
+}
