814523fb0d263a17188cacf1cea0f8019db1c9b0
Ormanus <osalminenster@gmail.com>
2016-07-29 16:04:44 +0300

    Shop update

--- Project Elements/Assets/Shop Screen/GoldDisplay.cs
+++ Project Elements/Assets/Shop Screen/GoldDisplay.cs
@@ -5,19 +5,27 @@ public class GoldDisplay : MonoBehaviour {
 
     public Transform gold;
     public Transform health;
+    public Transform mana;
+    public Transform speed;
 
     private Text goldText;
     private Text healthText;
+    private Text manaText;
+    private Text speedText;
 
     // Use this for initialization
     void Start () {
         goldText = gold.gameObject.GetComponent<Text>();
         healthText = health.gameObject.GetComponent<Text>();
+        manaText = mana.gameObject.GetComponent<Text>();
+        speedText = speed.gameObject.GetComponent<Text>();
     }
 
 	// Update is called once per frame
 	void Update () {
         goldText.text = " Gold: " + Inventory.money;
         healthText.text = " Health: " + PlayerHealth.Playerhealth + "/" + Inventory.maxHealth;
+        manaText.text = " Max. Mana: " + Inventory.maxMana + ", Regen: " + Inventory.manaRegen;
+        speedText.text = " Speed: " + Inventory.nopeus;
     }
 }
